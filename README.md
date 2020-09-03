65b17e5815671e704af3fb724fdbc97470f9b197
jbales20 <69781414+jbales20@users.noreply.github.com>
2020-09-03 00:29:22 -0400

    third commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # book-read-calculator
+# This Calculator is designed to get you the 
+# estimated read time for a book
+# based off the user's given information.
+# Programming language used - Python 3.7.9
+
+# Edit Log
+# First commit, added README.md
+# Second commit added Calculator.py
+# Third commit fixed README.md after text was not apeparing
+# Third commit also updated Calculator.py to offer two calculation options for the user
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
--- calculator.py
+++ calculator.py
@@ -1,16 +1,59 @@
-# This input asks for the user's average reading speed
-readSpeed = input("What is your average reading speed in words per minute? ")
-#This asks for the user the amount of pages in the book
-pages = input("What is the amount of pages in the book? ")
-#This asks the user a rough estimate for the amount of words per page.
-wordsPpage = input("Give me a rough estimate about the amount of words per page. ")
+# Option A -------------------------------------------------------------------------------------------------
+def optionA():
+    # Finds out the users' prefered reading time.
+    time = input("How many days do you have to finish the reading? ")
+    timeRequest = input("How many days a week do you wish to read? ")
+    minutes, hours = reading()
+    print()
+    #Converts time value into weeks value
+    weeks = int(time)/7
+    #Calculates how many days you are and are not reading based off time and timeRequest
+    daysNotReading = abs((int(timeRequest)*weeks) - int(time))
+    daysNotReading = daysNotReading - (daysNotReading%1)
+    daysReading = int(time) - int(daysNotReading)
+    #Gets the total minutes for reading
+    minutes = minutes + (hours*60)
+    #Gets the amount of minutes for each day. Cast as int to round it
+    minsPerDay = int(minutes/daysReading)+1
+    print("The book will take around " + str(minsPerDay) + " minutes each day, for you to read across " + str(daysReading) + " days of reading.")
+# Option A -------------------------------------------------------------------------------------------------
 
 
-#The script calculates it here.
-aprox = (int(pages)*int(wordsPpage))/int(readSpeed)
+# Option B --------------------------------------------------------------------------------------------------
+def optionB():
+    #This calls the function reading and sets it to the two variables listed.
+   minutes, hours = reading()
+   #Blank Line
+   print()
+   #Prints the results
+   print("The book will take you about " + str(hours) + " hours and " + str(minutes) + " minutes to read.")
 
-#This script converts the minutes into hours.
-minutes = aprox%60
-hours = (aprox - minutes)/60
+# Option B --------------------------------------------------------------------------------------------------
 
-print("The book will take you about " + str(hours) + " hours and " + str(minutes) + " minutes to read.")
+#Reading Time
+def reading():
+    # This input asks for the user's average reading speed
+    readSpeed = input("What is your average reading speed in words per minute? ")
+    #This asks for the user the amount of pages in the book
+    pages = input("What is the amount of pages in the book? ")
+    #This asks the user a rough estimate for the amount of words per page.
+    wordsPpage = input("Give me a rough estimate about the amount of words per page. ")
+    #The script calculates it here.
+    aprox = (int(pages)*int(wordsPpage))/int(readSpeed)
+    #Line below gets the remaining minutes, the one below that subtracts it from total to get the total hours in minutes and then converts into hours.
+    minutes = aprox%60
+    hours = (aprox - minutes)/60
+    return minutes, hours
+
+
+
+choice = input("Create A Reading Schedule or need an estimate for how long it'll take to read (Type '1' or '2' to choose) ")
+# Determines which option the user chooses. 
+if choice == "1":
+    print()
+    optionA()
+
+if choice == "2":
+    print()
+    optionB()
+
